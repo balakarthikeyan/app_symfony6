d512c1318261b09bb585abc4f0d297ca2f95faf8
Balakarthikeyan <balakarthikeyan07@gmail.com>
2024-07-22 17:08:03 +0530

    Twig & Profiler, Maker Added

---
+++ app/src/Controller/HelloController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HelloController extends AbstractController
+{
+
+    private array $messages = [
+        ['message' => 'Hello', 'created' => '2022/06/12'],
+        ['message' => 'Hi', 'created' => '2022/04/12'],
+        ['message' => 'Bye!', 'created' => '2021/05/12']
+    ];
+
+    public function indexWithResponse(): Response
+    {
+        return $this->render(
+            'hello/index.html.twig',
+            [
+                'messages' => $this->messages,
+                'limit' => 3
+            ]
+        );
+    }
+
+    #[Route('/welcome', name: 'app_welcome')]
+    public function indexWithJsonResponse(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/HelloController.php',
+        ]);
+    }
+
+    #[Route('/messages/{id<\d+>}', name: 'app_show_one')]
+    public function showOne(int $id): Response
+    {
+        return $this->render(
+            'hello/show_one.html.twig',
+            [
+                'message' => $this->messages[$id]
+            ]
+        );
+        // return new Response( @implode(', ', $this->messages) );
+    }
+}
