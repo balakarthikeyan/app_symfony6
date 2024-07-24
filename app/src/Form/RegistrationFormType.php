57d4dd2f32b6150c199254590bad8251b9180845
Balakarthikeyan <balakarthikeyan07@gmail.com>
2024-07-24 17:23:10 +0530

    Added embedded form & Like function

--- app/src/Form/RegistrationFormType.php
+++ app/src/Form/RegistrationFormType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\User;
+use App\Form\UserProfileType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -52,6 +53,7 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
+            ->add('userProfile', UserProfileType::class)
         ;
     }
 
